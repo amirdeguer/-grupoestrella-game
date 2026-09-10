4d7d17f2f4226db97108356e320432a1d01d0507
amirdeguer <66292653+amirdeguer@users.noreply.github.com>
2026-09-10 09:59:00 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='grupo-estrella-ultimate-v1';
-const ASSETS=['./','./index.html','./style.css','./game.js','./manifest.json','./assets/app-icon.svg','./assets/icon-192.png','./assets/icon-512.png','./assets/apple-touch-icon.png','./assets/syringe.svg','./assets/coffee.svg','./assets/amir.svg','./assets/florencia.svg','./assets/angela.svg','./assets/aline.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
